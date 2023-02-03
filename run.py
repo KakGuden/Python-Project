24e60c6d648d300aebe58afd3339f680c4f120bd
KakGuden <Blugy123@gmail.com>
2023-02-03 14:16:52 +0100

    working on levels

--- run.py
+++ run.py
@@ -1,29 +1,38 @@
 import random
 
-num1 = random.randint(1,10)
-num2 = random.randint(1,10)
+easy1 = random.randint(1,10)
+easy2 = random.randint(1,10)
+normal1 = random.randint(11,111)
+normal2 = random.randint(11,111)
 
 def addition():
-    answer = int(input(f"What is {num1} + {num2}?"))
-    correct = num1 + num2
-    if answer == correct:
-        print("well done")
+    difficulty = input("Select between Easy,Normal or Hard")
+    if difficulty == "Easy":
+        answer = int(input(f"What is {easy1} + {easy2}?"))
+        correct = easy1 + easy2
+        if answer == correct:
+            print("Well done")
+    elif difficulty == "Normal":
+        answer = int(input(f"What is {normal1} + {normal2}?"))
+        correct = normal1 + normal2
+        if answer == correct:
+            print("Well done")
 
 def subtraction():
-    answer = int(input(f"What is {num1} - {num2}?"))
-    correct = num1 - num2
+    answer = int(input(f"What is {easy1} - {easy2}?"))
+    correct = easy1 - easy2
     if answer == correct:
         print("well done")
 
 def multiplication():
-    answer = int(input(f"What is {num1} * {num2}?"))
-    correct = num1 * num2
+    answer = int(input(f"What is {easy1} * {easy2}?"))
+    correct = easy1 * easy2
     if answer == correct:
         print("well done")
 
 def division():
-    answer = int(input(f"What is {num1} / {num2}?"))
-    correct = num1 / num2
+    answer = int(input(f"What is {easy1} / {easy2}?"))
+    correct = easy1 / easy2
     if answer == correct:
         print("well done")
 
