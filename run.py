2b81abe43a29482aad5ce84e02422f5d9e1f1b10
KakGuden <Blugy123@gmail.com>
2023-02-03 14:29:39 +0100

    added harder mode

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ easy1 = random.randint(1,10)
 easy2 = random.randint(1,10)
 normal1 = random.randint(11,111)
 normal2 = random.randint(11,111)
-
+hard1 = round(random.uniform(1, 100),1)
+hard2 = round(random.uniform(1, 100),1)
 def addition():
     difficulty = input("Select between Easy,Normal or Hard")
     if difficulty == "Easy":
@@ -15,6 +16,11 @@ def addition():
     elif difficulty == "Normal":
         answer = int(input(f"What is {normal1} + {normal2}?"))
         correct = normal1 + normal2
+        if answer == correct:
+            print("Well done")
+    elif difficulty == "Hard":
+        answer = int(input(f"What is {hard1} + {hard2}?"))
+        correct = hard1 + hard2
         if answer == correct:
             print("Well done")
 
