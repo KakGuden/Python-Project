48594ba542368ae76c4423f19ad7b50741068683
KakGuden <Blugy123@gmail.com>
2023-02-03 13:51:25 +0100

    added more gamemodes

--- run.py
+++ run.py
@@ -7,5 +7,35 @@ def addition():
     answer = int(input(f"What is {num1} + {num2}?"))
     correct = num1 + num2
     if answer == correct:
-     print("well done")
-addition()
+        print("well done")
+
+def subtraction():
+    answer = int(input(f"What is {num1} - {num2}?"))
+    correct = num1 - num2
+    if answer == correct:
+        print("well done")
+
+def multiplication():
+    answer = int(input(f"What is {num1} * {num2}?"))
+    correct = num1 * num2
+    if answer == correct:
+        print("well done")
+
+def division():
+    answer = int(input(f"What is {num1} / {num2}?"))
+    correct = num1 / num2
+    if answer == correct:
+        print("well done")
+
+def game():
+    gamemode = input("Select gamemode")
+    if gamemode == "1":
+        addition()
+    elif gamemode == "2":
+        subtraction()
+    elif gamemode == "3":
+        multiplication()
+    elif gamemode == "4":
+        division()
+
+game()
