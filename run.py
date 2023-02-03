96b839001effcae48026b365ed6739fc8be8619a
KakGuden <Blugy123@gmail.com>
2023-02-03 23:24:51 +0100

    moving numbers into loops to make it able to replay

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 import random
 
-easy1 = random.randint(1,10)
-easy2 = random.randint(1,10)
-normal1 = random.randint(11,111)
-normal2 = random.randint(11,111)
-hard1 = round(random.uniform(1, 100),1)
-hard2 = round(random.uniform(1, 100),1)
+
+
 def addition():
     difficulty = input("Select between Easy,Normal or Hard")
     if difficulty == "Easy":
@@ -14,41 +10,38 @@ def addition():
         normalAddition()
     elif difficulty == "Hard":
         hardAddition()
+
 def easyAddition():
+    easy1 = random.randint(1,10)
+    easy2 = random.randint(1,10)
     answer = int(input(f"What is {easy1} + {easy2}?"))
     correct = easy1 + easy2
     if answer == correct:
         print("Well done")
+        nextGame = input("Press 1 to go again or 2 to switch mode")
+        if nextGame == "1":
+            easyAddition()
+        elif nextGame == "2":
+            game()
+
 def normalAddition():
-    answer = int(input(f"What is {easy1} + {easy2}?"))
-    correct = easy1 + easy2
+    normal1 = random.randint(11,111)
+    normal2 = random.randint(11,111)
+    answer = int(input(f"What is {normal1} + {normal2}?"))
+    correct = normal1 + normal2
     if answer == correct:
         print("Well done")
+
 def hardAddition():
-    answer = int(input(f"What is {easy1} + {easy2}?"))
-    correct = easy1 + easy2
+    hard1 = round(random.uniform(1, 100),1)
+    hard2 = round(random.uniform(1, 100),1)
+    answer = int(input(f"What is {hard1} + {hard2}?"))
+    correct = hard1 + hard2
     if answer == correct:
         print("Well done")
-
-def subtraction():
-    answer = int(input(f"What is {easy1} - {easy2}?"))
-    correct = easy1 - easy2
-    if answer == correct:
-        print("well done")
-
-def multiplication():
-    answer = int(input(f"What is {easy1} * {easy2}?"))
-    correct = easy1 * easy2
-    if answer == correct:
-        print("well done")
-
-def division():
-    answer = int(input(f"What is {easy1} / {easy2}?"))
-    correct = easy1 / easy2
-    if answer == correct:
-        print("well done")
 
 def game():
+    
     gamemode = input("Select gamemode")
     if gamemode == "1":
         addition()
@@ -57,6 +50,5 @@ def game():
     elif gamemode == "3":
         multiplication()
     elif gamemode == "4":
-        division()
-
+        division()  
 game()
