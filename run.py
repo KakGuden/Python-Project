adedd33839a4202ce33c25fc91b1e21381d78af1
KakGuden <Blugy123@gmail.com>
2023-02-03 12:40:27 +0100

    trying stuff

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def game():
+    print("please select game mode")
+    input("1 for addition, 2 for subtraction, 3 for multiplication and 4 for division")
+game()
+
+def addition():
+    num1 = random.randint(1,10)
+    print(num1)
+addition()
